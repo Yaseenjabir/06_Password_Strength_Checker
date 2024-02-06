8ef4f128810f735fb83d51d5818e173fc6e4c5f8
Yaseenjabir <yaseenkhan1235476@gmail.com>
2024-02-06 02:48:41 +0000

    version 1.1.0

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-const inputfield = document.querySelector('#password');
-const outputfield = document.querySelector('#output');
+// Selecting elements
+const input = document.querySelector("input");
+const lowerCase = document.querySelector("#LC");
+const UpperCase = document.querySelector("#UC");
+const number = document.querySelector("#NUM");
+const symbol = document.querySelector("#SYM");
 
 
-inputfield.addEventListener('input', function() {
-    let password = inputfield.value;
-    if (password.length < 8) {
-        outputfield.innerText = 'Password is too short';
-        outputfield.style.color = 'red';
-    }else {
-        if( password.search(/[a-z]/) == -1 ) {
-            outputfield.innerText = 'Password is missing a lowercase letter';
-            outputfield.style.color = 'red';
-        }else if (password.search(/[A-Z]/) == -1){
-            outputfield.innerText = 'Password is missing a Uppercase letter';
-            outputfield.style.color = 'red';
-        }else if (password.search(/[0-9]/) == -1){
-            outputfield.innerText = 'Password is missing a Numeric letter';
-            outputfield.style.color = 'red';
-        }else if (password.search(/[!\@\#\$\%\^\&\*\(\)\_\+\{\}\:\"\<\>\?\|\[\]\\\;\'\,\.]/) == -1){
-            outputfield.innerText = 'Password is missing a Special Character letter';
-            outputfield.style.color = 'red';
-        }
-        else {
-            outputfield.innerText = 'Password is strong';
-            outputfield.style.color = 'green';
-        }
+// const password = input.value;
 
+
+// Adding Event Listner to input box
+input.addEventListener('input', () => {
+  const password = input.value;
+
+  // Checking user input
+  if (password === "") {
+    input.placeholder = "Enter Password here";
+    lowerCase.style.backgroundColor = "white";
+    UpperCase.style.backgroundColor = "white";
+    number.style.backgroundColor = "white";
+    symbol.style.backgroundColor = "white";
+
+
+  } else{
+    //LowerCase Conditions
+    if (password.search(/[a-z]/) !== -1) {
+      lowerCase.style.backgroundColor = "green";
     }
-});
+    if (password.search(/[a-z]/) === -1) {
+      lowerCase.style.backgroundColor = "white";
+    }
+
+    //UpperCase Conditions
+    if (password.search(/[A-Z]/) !== -1){
+      UpperCase.style.backgroundColor = "green";
+    }
+    if (password.search(/[A-Z]/) === -1){
+      UpperCase.style.backgroundColor = "white";
+    }
+
+    //Numbers Conditions
+    if(password.search(/[0-9]/) !== -1){
+      number.style.backgroundColor = "green";
+    }
+    if(password.search(/[0-9]/) === -1){
+      number.style.backgroundColor = "white";
+    }
+
+    //Symbols Conditions
+    if(password.search(/[`\~\!\@\#\$\%\^\&\*\(\)\_\+\=\-\:\;\"\'\<\,\>\.\?\/]/) !== -1){
+      symbol.style.backgroundColor = "green";
+    } 
+    if(password.search(/[`\~\!\@\#\$\%\^\&\*\(\)\_\+\=\-\:\;\"\'\<\,\>\.\?\/]/) === -1){
+      symbol.style.backgroundColor = "white";
+    } 
+  } 
+})
+
+// Empty the input when refresh the browser
+window.addEventListener("beforeunload", ()=>{
+  input.value = ''
+})
